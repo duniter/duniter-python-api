71b0772b9a1df3753cec58de02618af3a97fb1c3
Inso <insomniak.fr@gmail.com>
2016-01-29 19:21:49 +0100

    Fix missed release

--- ucoinpy/__init__.py
+++ ucoinpy/__init__.py
@@ -21,7 +21,7 @@ PROTOCOL_VERSION="1"
 MANAGED_API=["BASIC_MERKLED_API"]
 
 __author__      = 'Caner Candan & inso'
-__version__     = '0.14.1'
+__version__     = '0.14.1.post1'
 __nonsense__    = 'uCoin'
 
 from . import api, documents, key
