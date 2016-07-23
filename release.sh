96ce1926b1b66e50acf24c3996c3d3b0b033e3a3
inso <insomniak.fr@gmaiL.com>
2016-07-23 18:38:45 +0200

    Fix release script

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@
 current=`grep -P "__version__     = \'\d+.\d+.\d+(\w*)\'" duniterpy/__init__.py | grep -oP "\d+.\d+.\d+(\w*)"`
 echo "Current version: $current"
 
-if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]+$ ]]; then
+if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]*$ ]]; then
   sed -i "s/__version__     = '$current'/__version__     = '$1'/g" duniterpy/__init__.py
   git commit duniterpy/__init__.py -m "$1"
   git tag "$1" -a -m "$1"
