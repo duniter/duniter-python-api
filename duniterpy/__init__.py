9a3c50751f5f57fd1c2732ba73598d4b406417b8
inso <insomniak.fr@gmaiL.com>
2016-08-19 06:31:22 +0200

    0.30.0

--- duniterpy/__init__.py
+++ duniterpy/__init__.py
@@ -21,7 +21,7 @@ PROTOCOL_VERSION="1"
 MANAGED_API=["BASIC_MERKLED_API"]
 
 __author__      = 'Caner Candan & inso'
-__version__     = '0.20.3'
+__version__     = '0.30.0'
 __nonsense__    = 'duniter'
 
 from . import api, documents, key
