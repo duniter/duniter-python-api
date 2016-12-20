1231d7069b53bc2c57803c6fef1e7f2922d1365d
inso <insomniak.fr@gmaiL.com>
2016-12-20 08:39:49 +0100

    Remove requirements

--- setup.py
+++ setup.py
@@ -3,40 +3,6 @@ import mirage
 import os
 import re
 
-def which(program):
-    """
-    Detect whether or not a program is installed.
-    Thanks to http://stackoverflow.com/a/377028/70191
-    """
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-
-    fpath, _ = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ['PATH'].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-
-    return None
-
-EDITABLE_REQUIREMENT = re.compile(r'^-e (?P<link>(?P<vcs>git|svn|hg|bzr).+#egg=(?P<package>.+)-(?P<version>\d(?:\.\d)*))$')
-
-install_requires = []
-dependency_links = []
-
-for requirement in (l.strip() for l in open('requirements.txt')):
-    match = EDITABLE_REQUIREMENT.match(requirement)
-    if match:
-        assert which(match.group('vcs')) is not None, \
-            "VCS '%(vcs)s' must be installed in order to install %(link)s" % match.groupdict()
-        install_requires.append("%(package)s==%(version)s" % match.groupdict())
-        dependency_links.append(match.group('link'))
-    else:
-        install_requires.append(requirement)
 
 setup(
     name='duniter-mirage',
@@ -66,7 +32,5 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Topic :: Communications",
     ],
-    install_requires=install_requires,
-    dependency_links=dependency_links
 
 )
