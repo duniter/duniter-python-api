1314f1ae936f22d080dafbf29202b364924de407
inso <insomniak.fr@gmaiL.com>
2017-03-29 08:18:45 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,18 @@ setup(
 
     description="A python mock server of [duniter](https://github.com/duniter/duniter) API",
 
-    long_description=open('README.md').read(),
+    long_description="""
+    # duniter-mirage
+A duniter mirage is a fake server for python testing
+
+To install duniter-mirage :
+
+```
+pip install duniter-mirage
+```
+
+Usage is described in example.py.
+""",
 
     # Active la prise en compte du fichier MANIFEST.in
     include_package_data=True,
