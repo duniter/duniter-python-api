49393ef28ecc406adbffa363ac81b0b1a1312fe2
Vincent Texier <vit@free.fr>
2019-03-30 20:30:47 +0100

    [enh] enhance release.sh to update documentation version

--- release.sh
+++ release.sh
@@ -5,8 +5,13 @@ current=`grep -P "__version__     = \'\d+.\d+.\d+(\w*)\'" duniterpy/__init__.py 
 echo "Current version: $current"
 
 if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]*$ ]]; then
+  # update version in duniterpy
   sed -i "s/__version__     = '$current'/__version__     = '$1'/g" duniterpy/__init__.py
-  git commit duniterpy/__init__.py -m "$1"
+  # update version in documentation configuration
+  sed -i "s/version = '$current'/version = '$1'/g" docs/conf.py
+  sed -i "s/release = '$current'/release = '$1'/g" docs/conf.py
+  # commit changes and add version tag
+  git commit duniterpy/__init__.py docs/conf.py -m "$1"
   git tag "$1" -a -m "$1"
 else
   echo "Wrong version format"
