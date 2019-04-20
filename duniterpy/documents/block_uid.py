f7ad0651fe227e50f2f6558083a693f0550c6352
Moul <moul@moul.re>
2019-04-20 21:50:57 +0200

    [enh] block_uid: Use NotImplemented

--- duniterpy/documents/block_uid.py
+++ duniterpy/documents/block_uid.py
@@ -56,22 +56,22 @@ class BlockUID:
 
     def __lt__(self, other: object) -> bool:
         if not isinstance(other, BlockUID):
-            return False
+            return NotImplemented
         return self.number < other.number
 
     def __gt__(self, other: object) -> bool:
         if not isinstance(other, BlockUID):
-            return False
+            return NotImplemented
         return self.number > other.number
 
     def __le__(self, other: object) -> bool:
         if not isinstance(other, BlockUID):
-            return False
+            return NotImplemented
         return self.number <= other.number
 
     def __ge__(self, other: object) -> bool:
         if not isinstance(other, BlockUID):
-            return False
+            return NotImplemented
         return self.number >= other.number
 
     def __hash__(self) -> int:
