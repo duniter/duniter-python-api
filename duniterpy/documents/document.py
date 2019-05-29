ec55b8fbd728b02fb1e9be9f9b30c13d648d6140
vtexier <vit@free.fr>
2019-05-29 18:02:33 +0200

    [enh] #91 fix pylint alerts in documents.document module

--- duniterpy/documents/document.py
+++ duniterpy/documents/document.py
@@ -80,7 +80,7 @@ class Document:
         self.signatures = []
         for key in keys:
             signing = base64.b64encode(key.signature(bytes(self.raw(), 'ascii')))
-            logging.debug("Signature : \n{0}".format(signing.decode("ascii")))
+            logging.debug("Signature : \n%s", signing.decode("ascii"))
             self.signatures.append(signing.decode("ascii"))
 
     def raw(self) -> str:
