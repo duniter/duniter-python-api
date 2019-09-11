b05c6dec6efb3b195adea3b703881c433a1377d6
Moul <moul@moul.re>
2019-09-11 07:25:01 +0200

    [mod] #107: Use direclty the version number into setup.py:version
    - Put current version into setup.py
    - Change release script to update this field

--- release.sh
+++ release.sh
@@ -7,11 +7,13 @@ echo "Current version: $current"
 if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]*$ ]]; then
   # update version in duniterpy
   sed -i "s/__version__ = \"$current\"/__version__ = \"$1\"/g" duniterpy/__init__.py
+  # update version in setup.py
+  sed -i "s/version=\"$current\",/version=\"$1\",/" setup.py
   # update version in documentation configuration
   sed -i "s/version = '$current'/version = '$1'/g" docs/conf.py
   sed -i "s/release = '$current'/release = '$1'/g" docs/conf.py
   # commit changes and add version tag
-  git commit duniterpy/__init__.py docs/conf.py -m "$1"
+  git commit setup.py duniterpy/__init__.py docs/conf.py -m "$1"
   git tag "$1" -a -m "$1"
 else
   echo "Wrong version format"
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ for requirement in (l.strip() for l in open('requirements.txt')):
 setup(
     name='duniterpy',
 
-    version=duniterpy.__version__,
+    version="0.55.1",
 
     packages=find_packages(exclude=["tests*"]),
 
