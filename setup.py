9bf3496be7881344a8fbe8ad42fca42ffa9d9d0b
vtexier <vit@free.fr>
2020-03-17 19:46:16 +0100

    0.1.50

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='duniter-mirage',
-
-    version="0.1.15",
-
+    name="duniter-mirage",
+    version="0.1.50",
     packages=find_packages(),
-
     author="inso",
-
     author_email="inso@tuta.io",
-
     description="A python mock server of [duniter](https://github.com/duniter/duniter) API",
-
     long_description="""
     # duniter-mirage
 A duniter mirage is a fake server for python testing
@@ -25,11 +19,9 @@ pip install duniter-mirage
 
 Usage is described in example.py.
 """,
-
     # Active la prise en compte du fichier MANIFEST.in
     include_package_data=True,
-    url='https://github.com/Insoleet/mirage',
-
+    url="https://github.com/Insoleet/mirage",
     classifiers=[
         "Programming Language :: Python",
         "Development Status :: 4 - Beta",
@@ -39,6 +31,5 @@ Usage is described in example.py.
         "Programming Language :: Python :: 3.5",
         "Topic :: Communications",
     ],
-    dependency_links=("git+https://github.com/duniter/duniter-python-api.git@dev",)
-
+    dependency_links=("git+https://github.com/duniter/duniter-python-api.git@dev",),
 )
