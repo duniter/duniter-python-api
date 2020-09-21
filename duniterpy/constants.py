36a1522825830e814f2f767ba56be83cddf0247d
Moul <moul@moul.re>
2020-09-21 21:23:41 +0200

    [doc] #111: Define the copyright and license statements (runtime)

--- duniterpy/constants.py
+++ duniterpy/constants.py
@@ -1,3 +1,20 @@
+"""
+Copyright  2014-2020 Vincent Texier <vit@free.fr>
+
+DuniterPy is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DuniterPy is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 UID_REGEX = "[A-Za-z0-9_-]{2,100}"
 PUBKEY_REGEX = "(?![OIl])[1-9A-Za-z]{42,45}"
 SIGNATURE_REGEX = "[A-Za-z0-9+/]+(?:=|==)?"
