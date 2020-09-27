ebba1a239078a1bc7fc3d16c4e5bbacc1bfd1f83
Moul <moul@moul.re>
2020-09-27 13:41:28 +0200

    [doc] Fix regex to update the documentation version

--- release.sh
+++ release.sh
@@ -10,8 +10,8 @@ if [[ $1 =~ ^[0-9]+.[0-9]+.[0-9]+[0-9a-z]*$ ]]; then
   # update version in pyproject.toml
   poetry version "$1"
   # update version in documentation configuration
-  sed -i "s/version = '$current'/version = '$1'/g" docs/conf.py
-  sed -i "s/release = '$current'/release = '$1'/g" docs/conf.py
+  sed -i "s/version = \"$current\"/version = \"$1\"/g" docs/conf.py
+  sed -i "s/release = \"$current\"/release = \"$1\"/g" docs/conf.py
   # commit changes and add version tag
   git commit pyproject.toml duniterpy/__init__.py docs/conf.py -m "$1"
   git tag "$1" -a -m "$1"
